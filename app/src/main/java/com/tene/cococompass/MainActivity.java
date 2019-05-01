fded0919850a6fabe80252b7b10dc8290ea69334
avsheksingh <avsheksingh@outlook.com>
2019-05-01 22:24:34 +0530

    compass app

---
+++ app/src/main/java/com/tene/cococompass/MainActivity.java
@@ -0,0 +1,95 @@
+package com.tene.cococompass;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView iv_compass;
+    private float[] gravity = new float[3];
+    private float[] geomagnetic = new float[3];
+    private float azimuth = 0f;
+    private float currentAzimuth = 0f;
+    private SensorManager sensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv_compass = findViewById(R.id.iv_compass);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_GAME);
+
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        final float alpha = 0.97f;
+        synchronized (this){
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                gravity[0] = alpha*gravity[0] + (1-alpha)*event.values[0];
+                gravity[1] = alpha*gravity[1] + (1-alpha)*event.values[1];
+                gravity[2] = alpha*gravity[2] + (1-alpha)*event.values[2];
+            }
+
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+                geomagnetic[0] = alpha*geomagnetic[0] + (1-alpha)*event.values[0];
+                geomagnetic[1] = alpha*geomagnetic[1] + (1-alpha)*event.values[1];
+                geomagnetic[2] = alpha*geomagnetic[2] + (1-alpha)*event.values[2];
+            }
+
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R,I,gravity,geomagnetic);
+            if (success){
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R,orientation);
+                azimuth = (float) Math.toDegrees(orientation[0]);
+                azimuth = (azimuth+360)%360;
+
+
+                Animation animation = new RotateAnimation(-currentAzimuth, -azimuth,
+                        Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                currentAzimuth = azimuth;
+
+                animation.setDuration(500);
+                animation.setRepeatCount(0);
+                animation.setFillAfter(true);
+
+                iv_compass.startAnimation(animation);
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
